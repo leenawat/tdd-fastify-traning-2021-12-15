684c7f5774ba83d7af908486ca364000ec8a567d
Leenawat Papahom <leenawat.pa@gmail.com>
2021-12-15 10:16:18 +0700

    6. การใช้งาน config/xxx.json ร่วมกับ cross-env

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import fastify from 'fastify'
 import fp from 'fastify-plugin'
 import App from './app'
+import config from 'config'
 
-const port = 8080
+const port: any = config.get('server-port')
+const myEnv: any = config.get('my-env')
 
 const start = async () => {
+  // eslint-disable-next-line no-console
+  console.log({ port, myEnv })
   try {
     const app = fastify({
       logger: {
