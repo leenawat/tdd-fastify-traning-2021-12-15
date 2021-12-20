ec5dc21567885d847b32eff5dc13cef081090efa
Leenawat Papahom <leenawat.pa@gmail.com>
2021-12-20 13:48:18 +0700

    creates profile folder under upload folder

--- src/FileService.ts
+++ src/FileService.ts
@@ -1,10 +1,17 @@
+import path from 'path'
+
 const fs = require('fs')
 const config = require('config')
 
 const uploadDir = config.get('upload-dir')
+const profileDir = config.get('profile-dir')
+const profileFolder = path.join('.', uploadDir, profileDir)
 
 export const createFolders = () => {
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir)
+  }
+  if (!fs.existsSync(profileFolder)) {
+    fs.mkdirSync(profileFolder)
   }
 }
--- test/FileService.test.ts
+++ test/FileService.test.ts
@@ -1,19 +1,20 @@
 import { createFolders } from '../src/FileService'
 
 const fs = require('fs')
+const path = require('path')
 const config = require('config')
 
 const uploadDir = config.get('upload-dir')
+const profileDir = config.get('profile-dir')
+const profileFolder = path.join('.', uploadDir, profileDir)
 
 describe('createFolders', () => {
-  beforeEach(async () => {
-    if (fs.existsSync(uploadDir)) {
-      fs.unlinkSync(uploadDir)
-    }
-  })
-
   it('creates upload folder', () => {
     createFolders()
     expect(fs.existsSync(uploadDir)).toBe(true)
+  })
+  it('creates profile folder under upload folder', () => {
+    createFolders()
+    expect(fs.existsSync(profileFolder)).toBe(true)
   })
 })
